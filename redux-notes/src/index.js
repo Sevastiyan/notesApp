8a2da2107d44532e9429829da13104450e3e7130
Sevi <sevastiyan.tsv@gmail.com>
2022-10-05 17:34:00 +0900

    Part6-a

---
+++ redux-notes/src/index.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import App from './App'
+
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import noteReducer from './reducers/noteReducer'
+
+const store = createStore(noteReducer)
+
+store.dispatch({
+  type: 'NEW_NOTE',
+  data: {
+    content: 'the app state is in redux store',
+    important: true,
+    id: 1,
+  },
+})
+
+store.dispatch({
+  type: 'NEW_NOTE',
+  data: {
+    content: 'state changes are made with actions',
+    important: false,
+    id: 2,
+  },
+})
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
+    {' '}
+    <App />
+  </Provider>
+)
